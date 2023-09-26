4d7e51e0585684f7f14eb6605e736c4278c6f03c
Dilpreet <92905896+dfordp@users.noreply.github.com>
2023-09-26 21:53:39 +0530

    first draft ready

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -7,7 +7,7 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract('0x44fBBbBC0eEFd0e4c6E42da4e5D5fb614BB34386');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
